ebcdcc50540ffb1acda3b2fa5d9942dc4849783d
Ender-robot <2120299823@qq.com>
2025-10-30 00:01:15 +0800

    修正注释

--- components/peripheral/i2c.hpp
+++ components/peripheral/i2c.hpp
@@ -10,7 +10,7 @@ class I2c {
 
         bool init();
         bool write_byte_to_mem(uint8_t addr, uint8_t mem_addr, uint8_t data); // 向指定寄存器写一个字节
-        bool read_bytes_from_mem(uint8_t addr, uint8_t mem_addr, uint8_t* bytes_buf, size_t len); // 从指定寄存器读一个字节
+        bool read_bytes_from_mem(uint8_t addr, uint8_t mem_addr, uint8_t* bytes_buf, size_t len); // 从指定寄存器开始读取len个字节
 
     private:
         i2c_port_t m_i2c_id;
